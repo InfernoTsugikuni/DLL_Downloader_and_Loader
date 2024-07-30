6dd90f2ecb35ac440841ce791c4284c8a08f8596
Inferno <167332139+InfernoTsugikuni@users.noreply.github.com>
2024-07-30 23:04:23 +0200

    Update dll.cpp

--- Source code/dll.cpp
+++ Source code/dll.cpp
@@ -1,7 +1,5 @@
-// dll.cpp
 #include <windows.h>
 
-// This function will be exported and can be called from an executable.
 extern "C" __declspec(dllexport) void ShowMessageBox() {
     MessageBoxW(NULL,
      L"hi from a dll",
@@ -9,7 +7,6 @@ extern "C" __declspec(dllexport) void ShowMessageBox() {
        MB_ICONWARNING | MB_YESNOCANCEL);
 }
 
-// DLL entry point (optional for this example, but good practice to include)
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     return TRUE;
 }
